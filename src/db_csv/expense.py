57a1bddcdfcea83021e9621fdafdec684f9913b6
Tuan Le <leanhtuan030523@gmail.com>
2025-01-03 12:30:38 +0700

    Change file_path to global var. Create get_expenses function

--- src/db_csv/expense.py
+++ src/db_csv/expense.py
@@ -1,5 +1,5 @@
 import uuid
-from csv import writer
+from csv import writer, reader
 from dataclasses import dataclass, field
 from typing import Optional
 import os
@@ -14,11 +14,24 @@ class Expense:
     category_expand: Optional[str] = field(default=None)
     note: Optional[str] = field(default=None)
     
+file_path = os.path.join(os.path.dirname(__file__), 'expenses.csv')
 
 def add_expense_to_csv(expense: Expense):
-    file_path = os.path.join(os.path.dirname(__file__), 'expenses.csv')
 
     with open(file_path, 'a') as f:
         w = writer(f, lineterminator='\n')
         w.writerow([expense.id, expense.name, expense.amount, expense.category, expense.category_expand, expense.date, expense.note])
         f.close()
+        
+def get_expenses() -> list[Expense]:
+    expenses: list[Expense] = []
+    with open(file_path, 'r') as f:
+        r = reader(f)
+        
+        next(r, None)
+        
+        for row in r:
+            expense = Expense(id=row[0], name=row[1], amount=row[2], category=row[3], category_expand=row[4], date=row[5], note=row[6])
+            expenses.append(expense)        
+        
+    return expenses
