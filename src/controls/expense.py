caad5b90b60fa67db7fe15e3005d2957f3e90709
Tuan Le <leanhtuan030523@gmail.com>
2025-01-04 20:28:35 +0700

    Change ExpenseRow from ft.Row to ft.DataRow

--- src/controls/expense.py
+++ src/controls/expense.py
@@ -1,16 +1,12 @@
 import flet as ft
 from db_csv.expense import Expense
 
-class ExpenseRow(ft.Row):
+class ExpenseRow(ft.DataRow):
     def __init__(self, expense: Expense):
-        super().__init__()
-        
-        self.date = ft.Text(expense.date, width=300)
-        self.name = ft.Text(expense.name, width=300)
-        self.amount = ft.Text(expense.amount, width=300)
-        
-        self.controls = [
-            self.date,
-            self.name,
-            self.amount,
-        ]
+        super().__init__(
+            cells=[
+                ft.DataCell(ft.Text(expense.date)),
+                ft.DataCell(ft.Text(expense.name)),
+                ft.DataCell(ft.Text(expense.amount)),
+            ]
+        )
