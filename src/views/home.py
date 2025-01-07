15731e55651df9489d1d686142dc40da80a748dc
Tuan Le <leanhtuan030523@gmail.com>
2025-01-07 20:10:03 +0700

    Add button to navigate to "/plan/new"

--- src/views/home.py
+++ src/views/home.py
@@ -6,14 +6,18 @@ class HomeView(ft.View):
     def __init__(self, page: ft.Page):
         super().__init__()
         
+        self.page = page
         self.route = "/"
-        self.navigation_bar = NavBar(page)
+        self.navigation_bar = NavBar(self.page)
         self.controls = []
         
         
     def did_mount(self):
         if not get_active_plan():
-            self.controls = [ft.Text("You have no active plan")]
+            self.controls = [
+                ft.Text("You have no active plan. Press the button below to create a new plan"),
+                ft.ElevatedButton("Create Plan", on_click=lambda e: self.page.go("/plan/new"))
+            ]
             self.update()
         else:
             self.controls = [
