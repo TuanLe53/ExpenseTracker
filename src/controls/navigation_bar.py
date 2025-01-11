f5486594d2b296b345f45911bbbdeac96477008b
Tuan Le <leanhtuan030523@gmail.com>
2025-01-11 19:04:52 +0700

    Remove Calculate route

--- src/controls/navigation_bar.py
+++ src/controls/navigation_bar.py
@@ -1,15 +1,15 @@
 import flet as ft 
 
 class NavBar(ft.NavigationBar):
-    def __init__(self, page: ft.Page):
+    def __init__(self, page: ft.Page, selected_index: int | None = None):
         super().__init__()
         
         self.page = page
+        self.selected_index = selected_index
         
         self.destinations = [
             ft.NavigationBarDestination(icon=ft.Icons.HOME, label="Home"),
             ft.NavigationBarDestination(icon=ft.Icons.ADD, label="Add Expense"),
-            ft.NavigationBarDestination(icon=ft.Icons.CALCULATE_ROUNDED, label="Calculate"),
         ]
         
         self.on_change = lambda e: self.navigate_to(e)
@@ -19,5 +19,3 @@ class NavBar(ft.NavigationBar):
             self.page.go("/")
         elif e.data == "1":
             self.page.go("/expense/new")
-        elif e.data == "2":
-            self.page.go("/calculate")
