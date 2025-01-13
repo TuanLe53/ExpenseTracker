e28f86be94a0ccd82f6314e6cd3e3e59577763ec
Tuan Le <leanhtuan030523@gmail.com>
2025-01-13 22:12:15 +0700

    Create BaseModel, PlanModel, ExpenseModel

---
+++ src/db/db.py
@@ -0,0 +1,32 @@
+from peewee import SqliteDatabase, ForeignKeyField, Model, DateField, FloatField, TextField, BooleanField, UUIDField, IntegerField
+import uuid
+
+db = SqliteDatabase('database.db')
+        
+def create_tables():
+    with db:
+        db.create_tables([PlanModel, ExpenseModel])
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+class PlanModel(BaseModel):
+    id = UUIDField(primary_key=True, default=uuid.uuid4)
+    name = TextField()
+    budget = IntegerField()
+    start_date = DateField()
+    end_date = DateField()
+    save_percentage = FloatField()
+    need_percentage = FloatField()
+    is_active = BooleanField()
+        
+class ExpenseModel(BaseModel):
+    id = UUIDField(primary_key=True, default=uuid.uuid4)
+    name = TextField()
+    amount = IntegerField()
+    category = TextField()
+    date = DateField()
+    category_expand = TextField(null=True)
+    note = TextField(null=True)
+    plan = ForeignKeyField(PlanModel, backref='expenses')
