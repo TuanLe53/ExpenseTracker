c21f98fdfd305fefe5eb18edb2cb01286c8df161
Tuan Le <leanhtuan030523@gmail.com>
2025-01-15 18:42:47 +0700

    Feat: Integrate Plan Management in HomeView
    - Added functionality to display details of the active plan, including name, total budget, and percentages for needs and savings.
    - Implemented 'Close Plan' button to finish the current plan using 'close_plan' from the database module.
    - Enhanced 'did_mount' to dynamically render UI based on the existence of an active plan.
    - Improved navigation bar initialization to include an active index parameter.

--- src/views/home.py
+++ src/views/home.py
@@ -1,29 +1,44 @@
 import flet as ft
 from controls.navigation_bar import NavBar
-from db.plan import get_active_plan
+from db.plan import get_active_plan, close_plan
 
 class HomeView(ft.View):
     def __init__(self, page: ft.Page):
         super().__init__()
         
         self.page = page
-        self.route = "/"
-        self.navigation_bar = NavBar(self.page)
+        self.navigation_bar = NavBar(self.page, 0)
+        
+        self.plan = get_active_plan()
+        
         self.controls = []
         
         
     def did_mount(self):
-        if not get_active_plan():
+        if self.plan == None:
             self.controls = [
                 ft.Text("You have no active plan. Press the button below to create a new plan"),
                 ft.ElevatedButton("Create Plan", on_click=lambda e: self.page.go("/plan/new"))
             ]
             self.update()
-        else:
+        else:            
             self.controls = [
-                ft.Text("Welcome to Tracker"),
-                ft.Text("This is a simple expense tracker app"),
-                ft.Text("You can start by adding your income and expenses"),
-                ft.Text("You can also calculate your expenses"),
+                ft.Text(self.plan.name),
+                ft.Text(f"Total: {self.plan.budget:,.0f}"),
+                ft.Row([
+                    ft.Text(f"Need: {self.plan.need_percentage}%"),
+                    ft.Text(f"Save: {self.plan.save_percentage}%")
+                ]),
+                ft.ElevatedButton("Close plan", on_click=self.finish_plan)
             ]
+            
+            
             self.update()
+            
+    def finish_plan(self, e):
+        close_plan()
+        self.controls = [
+            ft.Text("You have no active plan. Press the button below to create a new plan"),
+            ft.ElevatedButton("Create Plan", on_click=lambda e: self.page.go("/plan/new"))
+        ]
+        self.update()
