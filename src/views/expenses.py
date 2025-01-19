19e3dfe7e60d1e026dbceaa0666a1c02810caf7d
Tuan Le <leanhtuan030523@gmail.com>
2025-01-19 20:50:13 +0700

    Create simple ExpensesView

---
+++ src/views/expenses.py
@@ -0,0 +1,14 @@
+import flet as ft
+from controls.expense_list import ExpenseList
+from db.expense import get_expenses_from_current_plan
+
+class ExpensesView(ft.View):
+    def __init__(self, page: ft.Page):
+        
+        self.expenses = []
+        self.controls = []
+        
+    def did_mount(self):
+        self.expenses = get_expenses_from_current_plan
+        self.controls = [ExpenseList(self.expenses)]
+        self.update()
