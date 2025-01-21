9cfa5ba65e13d6349bd411b3dcd729326bcc37d1
Tuan Le <leanhtuan030523@gmail.com>
2025-01-21 19:30:00 +0700

    Feat: Integrate Expense Budget and List in HomeView
    - Added integration of expenses in the HomeView using 'get_expenses_from_current_plan'.
    - Implemented 'get_expense_budget' method to calculate and return the budget allocated for expenses based on the active plan's need percentage.
    - Integrated 'Expenses' control in the UI, dynamically displaying expenses and their budget.
    - Enhanced user experience with a scrollable view to accommodate multiple controls.

--- src/views/home.py
+++ src/views/home.py
@@ -1,6 +1,8 @@
 import flet as ft
 from controls.navigation_bar import NavBar
+from controls.expense import Expenses
 from db.plan import get_active_plan, close_plan
+from db.expense import get_expenses_from_current_plan
 
 class HomeView(ft.View):
     def __init__(self, page: ft.Page):
@@ -8,8 +10,10 @@ class HomeView(ft.View):
         
         self.page = page
         self.navigation_bar = NavBar(self.page, 0)
+        self.scroll = ft.ScrollMode.ALWAYS
         
         self.plan = get_active_plan()
+        self.expenses = []
         
         self.controls = []
         
@@ -22,6 +26,8 @@ class HomeView(ft.View):
             ]
             self.update()
         else:            
+            expense_budget = self.get_expense_budget()
+            
             self.controls = [
                 ft.Text(self.plan.name),
                 ft.Text(f"Total: {self.plan.budget:,.0f}"),
@@ -29,7 +35,8 @@ class HomeView(ft.View):
                     ft.Text(f"Need: {self.plan.need_percentage}%"),
                     ft.Text(f"Save: {self.plan.save_percentage}%")
                 ]),
-                ft.ElevatedButton("Close plan", on_click=self.finish_plan)
+                ft.ElevatedButton("Close plan", on_click=self.finish_plan),
+                Expenses(self.page, expense_budget, self.expenses)
             ]
             
             
@@ -42,3 +49,9 @@ class HomeView(ft.View):
             ft.ElevatedButton("Create Plan", on_click=lambda e: self.page.go("/plan/new"))
         ]
         self.update()
+        
+    def get_expense_budget(self) -> int:
+        self.expenses = get_expenses_from_current_plan()
+        expense_budget = (float(self.plan.need_percentage) / 100) * self.plan.budget
+        
+        return int(expense_budget)
