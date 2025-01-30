5418e5330de70b636aad6ba29598d7dffc8cfc7e
Tuan Le <leanhtuan030523@gmail.com>
2025-01-30 18:11:28 +0700

    Feat: Add  Component with Delete Functionality
    - Introduced 'ExpenseRow' class to encapsulate individual expense entries.
    - Each row includes date, name, amount, and a delete button.
    - Delete button removes the expense from the list and updates the parent view.

--- src/controls/expense_list.py
+++ src/controls/expense_list.py
@@ -1,5 +1,6 @@
+import uuid
 import flet as ft
-from db_csv.expense import Expense
+from db_csv.expense import Expense, delete_expense_by_id
 from .expense_row import ExpenseRow
 
 class ExpenseList(ft.DataTable):
@@ -14,3 +15,26 @@ class ExpenseList(ft.DataTable):
         
         
         self.rows = [ExpenseRow(expense, self) for expense in expenses]
+        
+class ExpenseRow(ft.DataRow):
+    def __init__(self, expense: Expense, parent):
+        self.parent = parent
+        super().__init__(
+            cells=[
+                ft.DataCell(ft.Text(expense.date)),
+                ft.DataCell(ft.Text(expense.name)),
+                ft.DataCell(ft.Text(expense.amount)),
+                ft.DataCell(ft.IconButton(
+                    icon=ft.Icons.DELETE_FOREVER_ROUNDED,
+                    icon_color="red500",
+                    icon_size=40,
+                    tooltip="Delete expense",
+                    on_click=lambda e: self.delete(e, expense.id)
+                ))
+            ]
+        )
+        
+    def delete(self, e, expense_id: uuid.UUID) -> None:
+        delete_expense_by_id(expense_id)
+        self.parent.rows.remove(self)
+        self.parent.update()
